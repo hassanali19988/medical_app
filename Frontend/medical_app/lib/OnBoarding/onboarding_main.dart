0811151c24c62bffc8eb8047ea00b7e5e4c3c351
Saif Alhaider <saifharith90@gmail.com>
2022-09-02 21:59:53 +0300

    added lottie package also initated onboarding

---
+++ Frontend/medical_app/lib/OnBoarding/onboarding_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingMain extends StatefulWidget {
+  OnBoardingMain({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingMain> createState() => _OnBoardingMainState();
+}
+
+class _OnBoardingMainState extends State<OnBoardingMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
